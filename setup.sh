f8d0937f9ef6891ee827ff8aea21f20f06ca4387
canpok1 <6isstrong@gmail.com>
2014-11-09 21:12:35 +0900

    リンク作成等を行うLinux用スクリプトを追加。

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ln -sf ~/dotfiles/_vimrc ~/.vimrc
+ln -sf ~/dotfiles/_gvimrc ~/.gvimrc
+ln -sfd ~/dotfiles/vimfiles ~/vimfiles
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+FirstDir=`pwd`
+cd `dirname $0`
+git submodule init
+git submodule update
+cd $FirstDir
