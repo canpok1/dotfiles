47dcbca9a0e58501bf83d8fe4ee32f61e5d5b9c1
canpok1 <6isstrong+github@gmail.com>
2014-11-09 21:33:10 +0900

    gitのコマンドを追加。

--- makeLinks.bat
+++ makeLinks.bat
@@ -3,3 +3,8 @@ mklink %USERPROFILE%\_vimrc %~dp0\_vimrc
 mklink %USERPROFILE%\_gvimrc %~dp0\_gvimrc
 mklink /d %USERPROFILE%\vimfiles %~dp0\vimfiles
 mklink %USERPROFILE%\.gitconfig %~dp0\.gitconfig
+
+pushd %~dp0
+git submodule init
+git submodule update
+popd
