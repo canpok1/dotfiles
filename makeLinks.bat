59dc724f09de41c52a6efe8d4e7bb36186bf09e7
canpok1 <6isstrong@gmail.com>
2014-11-09 20:38:11 +0900

    リンクを作成するバッチを追加。

---
+++ makeLinks.bat
@@ -0,0 +1,5 @@
+@echo off
+mklink %USERPROFILE%\_vimrc %~dp0\_vimrc
+mklink %USERPROFILE%\_gvimrc %~dp0\_gvimrc
+mklink /d %USERPROFILE%\vimfiles %~dp0\vimfiles
+mklink %USERPROFILE%\.gitconfig %~dp0\.gitconfig
