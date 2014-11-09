a0975e3be6e138ca3f8fdce22be61dbc8c016401
canpok1 <6isstrong@gmail.com>
2014-11-09 22:34:03 +0900

    vimfilesのリンク名を.vimに変更。

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
 ln -sf ~/dotfiles/_gvimrc ~/.gvimrc
-ln -sfd ~/dotfiles/vimfiles ~/vimfiles
+ln -sfd ~/dotfiles/.vim ~/vimfiles
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 FirstDir=`pwd`
