1b508d1e05778efe299aee3a51ffab586babe879
canpok1 <6isstrong@gmail.com>
2015-03-16 22:43:35 +0900

    コマンドのオプションのミスを修正

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
 ln -sf ~/dotfiles/_gvimrc ~/.gvimrc
-ln -sfd ~/dotfiles/vimfiles ~/.vim
+ln -sf ~/dotfiles/vimfiles ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 FirstDir=`pwd`
