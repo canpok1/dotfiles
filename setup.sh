57c5d950af29186559750d360f7027955b71ff55
canpok1 <6isstrong@gmail.com>
2015-03-22 01:02:08 +0900

    コメント追加

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+echo ---- dotfiles setup start ----
+echo make link
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
 ln -sf ~/dotfiles/_gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/vimfiles ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-
+ 
+echo submodule init and update
 FirstDir=`pwd`
 cd `dirname $0`
 git submodule init
 git submodule update
 cd $FirstDir
 
+echo vim init
 vim +":NeoBundleInstall" +":q"
+
+echo ---- dotfiles setup end ----
